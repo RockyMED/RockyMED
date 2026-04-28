700d3d9fb6bec26f16119012a676f2c80d6aaa1f
RockyMED <rocky.med@capcol.com.co>
2026-04-28 10:06:45 -0500

    2.3.0.1.

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,3 +2,5 @@ export const SUPABASE_URL = 'https://yqnosrmpjwxaujzaxdji.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlxbm9zcm1wand4YXVqemF4ZGppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNDcyMzksImV4cCI6MjA5MDcyMzIzOX0.FrHCqkz7D1ZQ8uMy1Y9dWEAjmGOQsjJGCh062ErKPZ4';
 export const SUPABASE_PROFILES_TABLE = 'profiles';
 export const EMPLOYEE_PORTAL_API_BASE = 'https://rocky-med.vercel.app';
+
+
